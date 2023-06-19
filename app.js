37bf6e5289ea1fba83c45f0911ce7adbb5a45b3f
LePeteMul <petemul80@gmail.com>
2023-06-19 11:23:34 +0200

    quete 08

--- app.js
+++ app.js
@@ -4,29 +4,40 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const { hashPassword } = require("./auth.js");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth");
+
+const userHandlers = require("./userHandlers");
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
 
+
+//utilisateur test : Bob,Gru, bob@mail.com, ville "Lo" langage "Kt" mot de passe:truc
+
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
-const userHandlers = require("./userHandlers");
+// const userHandlers = require("./userHandlers");
 
-app.get("/api/movies/", movieHandlers.getMovies);
+
+//routes publiques
+app.get("/api/movies/", verifyToken, movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-
 app.get("/api/users/", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
-
-app.post("/api/movies", movieHandlers.postMovie);
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
 app.post("/api/users", hashPassword, userHandlers.postUser);
 
+app.use(verifyToken);//mur d'authetification
+//routes protégées
+app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
-
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 app.delete("/api/users/:id", userHandlers.deleteUser);
 
